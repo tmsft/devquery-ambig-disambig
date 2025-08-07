bfd0402e7b4eae4cef72a80c0ba74becbf6ab47b
tmsft <t-tulikaarun@microsoft.com>
2025-08-07 15:55:37 +0530

    Update env.bat

--- Query_Ambig_Disambig/env.bat
+++ Query_Ambig_Disambig/env.bat
@@ -1,4 +1,3 @@
 @echo off
-SET ENDPOINT_URL=https://intern-project-openai.openai.azure.com/
-SET DEPLOYMENT_NAME=gpt-4o
-SET AZURE_OPENAI_API_KEY=6vIAgBoilozGdDjEfCKJu6f84b4un1gLYD9Cerc30LWKgZYk9AvaJQQJ99BGACYeBjFXJ3w3AAABACOGsZhb
+
+
