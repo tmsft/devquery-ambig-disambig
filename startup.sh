c983136ea5d90e760093dda4b52ef11ac170545f
tmsft <t-tulikaarun@microsoft.com>
2025-08-10 21:20:03 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 apt-get update
-apt-get install -y wget unzip
+apt-get install -y wget unzip curl
 
 # Install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Install Chromedriver
+# Install ChromeDriver
 CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip -d /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
 
-# Start Streamlit
-# $PORT is set by Azure Web Apps dynamically
+# Start Streamlit app
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
