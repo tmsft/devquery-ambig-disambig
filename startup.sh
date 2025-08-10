5b61cb98ac4ff693bfe6f3792334931e42a87bc7
tmsft <t-tulikaarun@microsoft.com>
2025-08-10 18:13:46 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+apt-get update
+apt-get install -y wget unzip
+
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Install Chromedriver
+CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
+
+# Start Streamlit
+# $PORT is set by Azure Web Apps dynamically
+streamlit run app.py --server.port $PORT --server.address 0.0.0.0
