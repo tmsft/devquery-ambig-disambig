da6d236be02afa79e93ee4f3861fa8ed7079e670
tmsft <t-tulikaarun@microsoft.com>
2025-08-10 21:53:43 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-set -e  # fail on error
-
-# Install Chrome
-apt-get update
-apt-get install -y wget unzip curl gnupg
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb || apt-get -f install -y
-
-# Install Chromedriver
-CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
-wget https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d /usr/local/bin/
-chmod +x /usr/local/bin/chromedriver
-
-# Start Streamlit
-exec streamlit run app.py --server.port $PORT --server.address 0.0.0.0
+streamlit run app.py --server.port $PORT --server.address 0.0.0.0
